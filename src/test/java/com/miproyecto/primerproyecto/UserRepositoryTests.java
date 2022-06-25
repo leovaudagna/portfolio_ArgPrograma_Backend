20595faa77c53bf71493ff0f68003b742ebba29e
Leonardo Vaudagna <leovaudagna92@gmail.com>
2022-06-25 01:06:54 -0300

    correcciones CORS

---
+++ src/test/java/com/miproyecto/primerproyecto/UserRepositoryTests.java
@@ -0,0 +1,15 @@
+package com.miproyecto.primerproyecto;
+
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Rollback(false)
+//public class UserRepositoryTests {
+//    @Autowired
+//    UserRepository repo;
+//    
+//    @Test
+//    public void testCreateUser(){
+//        PasswordEncoder
+//    }
+//    
+//}
